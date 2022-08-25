c3f9dc851d89e989fb6fcee0705d01813ef4394d
wr <86941613+wr0x00@users.noreply.github.com>
2022-08-25 15:19:18 +0800

    Update size.py

--- size.py
+++ size.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     c_=cdll.LoadLibrary("./forc.dll")
     files=input("输入字典名称：")
     string=input("输入包括的字符：")
-    lens=input("输入生成字符串位数：")
+    lens=input("输入生成字典位数：")
     c_.dic(files,string,lens)
